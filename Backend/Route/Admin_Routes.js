b256f2010cfa34c7cae9d903282d6d8c5f070c74
Likhith025 <alasyamlikhith2@gmail.com>
2025-09-24 18:49:00 +0530

    forget password

--- Backend/Route/Admin_Routes.js
+++ Backend/Route/Admin_Routes.js
@@ -4,14 +4,8 @@ import { protectAdmin } from "../Middleware/authContextAdmin.js";
 
 const AdminRoute = express.Router();
 
-// Root endpoint for adding admin
-//AdminRoute.post("/addit", addAdmin);
 
 AdminRoute.post("/addu", addAdmin);
 AdminRoute.post("/add",protectAdmin, addAdmin);
-
-// Route 2: Setup Admin Password (OTP verification)
-AdminRoute.post("/setup-password", setupAdminPassword);
-
 
 export default AdminRoute;
