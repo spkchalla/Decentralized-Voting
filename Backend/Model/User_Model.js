1df4b9d7588144d47f65ca12ad055cd3d7cc6a5d
Likhith025 <alasyamlikhith2@gmail.com>
2025-09-25 16:19:10 +0530

    Edited new Encryption

--- Backend/Model/User_Model.js
+++ Backend/Model/User_Model.js
@@ -20,16 +20,44 @@ const userSchema = new Schema({
         required: true
     },
     publicKey: {
-        type: String
+        type: String, // Store encrypted public key
+        required: true
+    },
+    publicKeyIV: {
+        type: String, // Store IV for public key encryption
+        required: true
+    },
+    publicKeyAuthTag: {
+        type: String, // Store auth tag for public key encryption
+        required: true
     },
     privateKey: {
-        type: String // store encrypted private key here
+        type: String, // Store encrypted private key
+        required: true
     },
     privateKeyIV: {
-        type: String // store AES IV in hex
+        type: String, // Store IV for private key encryption
+        required: true
+    },
+    privateKeyAuthTag: {
+        type: String, // Store auth tag for private key encryption
+        required: true
     },
     privateKeySalt: {
-        type: String // store salt used for AES key derivation in hex
+        type: String, // Store salt used for AES key derivation in hex
+        required: true
+    },
+    token: {
+        type: String, // Store encrypted token
+        required: true
+    },
+    tokenIV: {
+        type: String, // Store IV for token encryption
+        required: true
+    },
+    tokenAuthTag: {
+        type: String, // Store auth tag for token encryption
+        required: true
     },
     isVerified: {
         type: Boolean,
